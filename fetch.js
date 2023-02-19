b1b5abd9e76366633dd7b76696f863257dbc5fe3
boredcoder411 <taamarcos@gmail.com>
2023-02-19 10:02:21 +0100

    added small wrapper for a get fetch

--- fetch.js
+++ fetch.js
@@ -3,5 +3,11 @@ function jsonFetch(url, method, content) {
     method: method,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(content),
-  });
+  }).then((resp) => {return resp}).catch((error) => {return error});
+}
+
+function getFetch(url) {
+  fetch(url, {
+    method: "GET"
+  }).then((resp) => {return resp}).catch((error) => {return error});
 }
